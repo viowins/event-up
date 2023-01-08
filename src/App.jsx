85e9c2025a55c3118bbd3fa540612f7dc0213f52
Emre YARAŞIR <asdonur156@gmail.com>
2023-01-08 21:25:46 +0300

    yapı

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./components/Header"
+
 export default function App() {
   return (
+    <>
+    <Header />
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    </>
   )
 }
---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+export default function Logo() {
+  return (
+    <img src="/Logo.svg" draggable="false" />
+  ) 
+}
